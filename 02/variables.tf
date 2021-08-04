72e48633dd7b7f5b4cb85d985928ba536acf3ca6
Yacine BENAMRI <yacine.benamri@gmail.com>
2021-08-04 17:30:59 +0200

    add 02 new empty files

---
+++ 02/variables.tf
@@ -0,0 +1,36 @@
+variable "resource_01_group_name" {
+   default = "rg-01-terraform"
+}
+variable "vnet_01_name" {
+   default = "vnet-01-terraform"
+}
+variable  "subnet_01_name" {
+   default =  "subnet-01-terraform"
+}
+variable "nic_01_name" {
+  default = "nic-01-terraform"
+}
+variable "pip_01_name" {
+  default = "pip-01-terraform"
+}
+variable "nsg_01_name" {
+  default = "nsg-01-terraform"
+}
+variable "bd_01_name" {
+  default = "bd-01-terraform"
+}
+variable "stra_01_name" {
+  default = "stra-01-terraform"
+}
+variable "vm_01_name" {
+  default = "vm-01-terraform"
+}
+variable "location" {
+  default = "westeurope"
+}
+variable "os_disk_01_name" {
+  default = "os-disk-01-terraform"
+}
+variable "admin_user" {
+  default = "azureuser"
+}
